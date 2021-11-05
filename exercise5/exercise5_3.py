1376034e9a8d74e7a5787f1112bca290b8ce0940
hypnootis <tm.pasanen@outlook.com>
2021-11-05 11:32:57 +0200

    exercise5_3

--- exercise5/exercise5_3.py
+++ exercise5/exercise5_3.py
@@ -1,16 +1,17 @@
-PI = 1.31415926
+from math import pi
 
 
 while True:
-    radius = float(input("Give radius:\n"))
-    circle_area = PI * (radius ** 2)
-    print(circle_area)
-    choice = input("Do you want to continue? (y/n)\n")
-    
     try:
+        radius = float(input("Give radius:\n"))
+        circle_area = pi * (radius ** 2)
+        circle_area = round(circle_area, 2)
+        print(circle_area)
+        choice = input("Do you want to continue? (y/n)\n")
+
         if choice == "y":
             continue
         else:
             break
     except:
-        print("By not being able to follow basic instructions,\n you have doomed mankind. The program will now terminate.")
+        pass
