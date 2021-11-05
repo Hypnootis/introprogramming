c04c6a65ea82332f7d157ab91f0243908e47d8a5
hypnootis <tm.pasanen@outlook.com>
2021-11-05 13:53:32 +0200

    exercise5_5

---
+++ exercise5/exercise5_6.py
@@ -0,0 +1,6 @@
+movie = {"name": "Pulp Fiction",
+         "year": 1994,
+         "director": "Quentin Tarantino",
+         "genre": "Crime, Drama",
+         "duration": 154}
+print("WITH a loop:\n")
