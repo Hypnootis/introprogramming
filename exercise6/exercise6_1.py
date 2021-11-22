c28d4a78605bb35eae32a3ee4e60dd475f596c29
hypnootis <tm.pasanen@outlook.com>
2021-11-22 12:41:02 +0200

    exercise6_1

--- exercise6/exercise6_1.py
+++ exercise6/exercise6_1.py
@@ -4,10 +4,11 @@ try:
     biggest = 0
     for i in range(5):
         number = int(input("Give a number:\n"))
-        if number > biggest:
-            biggest = number
-        else:
-            pass
+        if number > 0:
+            if number > biggest:
+                biggest = number
+            else:
+                pass
     print(f"The biggest number from user: {biggest}")
 
 except:
