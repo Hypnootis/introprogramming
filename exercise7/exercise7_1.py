ae3b3b0b9346e0114e20b46fdb3de6f77ba378b1
hypnootis <tm.pasanen@outlook.com>
2021-12-07 13:58:56 +0200

    exercise7_1

---
+++ exercise7/exercise7_1.py
@@ -0,0 +1,30 @@
+cafe = {
+    "name": "Imaginary Cafe Ltd.",
+    "website": "https://edu.frostbit.fi/sites/cafe/en",
+    "categories": [
+        "cafe",
+        "tea",
+        "lunch",
+        "breakfast"
+    ],
+    "location": {
+        "city": "Rovaniemi",
+        "address": "Test address 22",
+        "zip_code": "FI-96100"
+    }
+}
+
+print(cafe["name"])
+print(cafe["location"]["address"])
+print(cafe["location"]["zip_code"], cafe["location"]["city"])
+print("\n")
+print(cafe["website"])
+
+separator = ", "
+text = "Services: "
+new_text = ""
+for i in cafe["categories"]:
+    new_text = separator.join(cafe["categories"])
+text += new_text
+
+print(text)
